0aa927a8bb06f02efeeecf9f08cf6a095839eeeb
VukZh <36912856+VukZh@users.noreply.github.com>
2019-01-15 17:17:34 +0600

    Update PQueueTest.java

--- PQueueTest.java
+++ PQueueTest.java
@@ -1,41 +1,39 @@
 package queuetest;
 
 public class PQueueTest {
+
     public static void main(String[] args) {
         {
-            PQueue<Integer> p1 = new PQueue<Integer>(5);
+            PQueue<Integer> p1 = new PQueue<Integer>(3);
             p1.enqueue(1, 000);
             p1.enqueue(2, 111);
             p1.enqueue(2, 222);
             p1.enqueue(2, 333);
             p1.enqueue(2, 444);
-            p1.enqueue(3, 777);
+            p1.enqueue(4, 777);
+            p1.enqueue(11, 999);
             p1.enqueue(1, 001);
-            p1.dequeue();
-            p1.dequeue();
-            p1.dequeue();
-            p1.dequeue();
-            p1.dequeue();
-            p1.dequeue();
-            p1.dequeue();
-            p1.dequeue();
+            p1.enqueue(1, 888);
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
+            System.out.println(p1.dequeue());
 
             System.out.println("---");
 
-            PQueue<String> p2 = new PQueue<String>(3);
-            p2.enqueue(1, "001");
-            p2.enqueue(2, "002");
-            p2.enqueue(2, "003");
-            p2.enqueue(2, "004");
-            p2.enqueue(2, "005");
-            p2.enqueue(3, "006");
-            p2.dequeue();
-            p2.dequeue();
-            p2.dequeue();
-            p2.dequeue();
-            p2.dequeue();
-            p2.dequeue();
-            p2.dequeue();
+            PQueue<String> p2 = new PQueue<String>();
+            p2.enqueue(1, "aaa");
+            p2.enqueue(10, "bbb");
+            System.out.println(p2.dequeue());
+            System.out.println(p2.dequeue());
+            System.out.println(p2.dequeue());
+
         }
     }
 }
